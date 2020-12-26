6f763cc0b3749523fc43fbd6012131da82aa19f2
Danillo Franco Camargo <danillo.brp@gmail.com>
2020-12-26 08:34:26 -0300

    Add Rules

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,21 @@
+import { response } from 'express';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface AllTransactionsResponse {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +25,41 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): AllTransactionsResponse {
+    const responseTransactions = this.transactions;
+
+    const responseBalance = this.getBalance();
+
+    return { transactions: responseTransactions, balance: responseBalance };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce<number>(
+      (accumulator, currentValue) =>
+        currentValue.type === 'income'
+          ? accumulator + currentValue.value
+          : accumulator,
+      0,
+    );
+
+    const outcome = this.transactions.reduce<number>(
+      (accumulator, currentValue) =>
+        currentValue.type === 'outcome'
+          ? accumulator + currentValue.value
+          : accumulator,
+      0,
+    );
+    const total: number = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
